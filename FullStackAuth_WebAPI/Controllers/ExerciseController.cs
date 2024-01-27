a9bf1c9fd3017ef029c22928e0b924cbcfb7b65d
djackson5995 <146674106+djackson5995@users.noreply.github.com>
2024-01-27 03:22:17 -0700

    created and tested both the routine and exercise controllers

---
+++ FullStackAuth_WebAPI/Controllers/ExerciseController.cs
@@ -0,0 +1,59 @@
+﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/exercises")]
+public class ExerciseController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public ExerciseController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public IActionResult CreateExercise([FromBody] Exercise exercise)
+    {
+
+        _context.Exercises.Add(exercise);
+        _context.SaveChanges();
+
+        return Ok(exercise);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateExercise(int id, [FromBody] Exercise updatedExercise)
+    {
+        var existingExercise = _context.Exercises.Find(id);
+
+        if (existingExercise == null)
+        {
+            return NotFound();
+        }
+
+
+        existingExercise.Type = updatedExercise.Type;
+        existingExercise.Weight = updatedExercise.Weight;
+        existingExercise.Reps = updatedExercise.Reps;
+        existingExercise.Sets = updatedExercise.Sets;
+
+        _context.SaveChanges();
+
+        return Ok(existingExercise);
+    }
+    [HttpGet("{id}")]
+    public IActionResult GetExercise(int id)
+    {
+        var exercise = _context.Exercises.Find(id);
+
+        if (exercise == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(exercise);
+    }
+
+}
---
+++ FullStackAuth_WebAPI/Controllers/RoutineController.cs
@@ -0,0 +1,44 @@
+﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/routines")]
+public class RoutineController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public RoutineController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public IActionResult CreateRoutine([FromBody] Routine routine)
+    {
+
+        _context.Routines.Add(routine);
+        _context.SaveChanges();
+
+        return Ok(routine);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateRoutine(int id, [FromBody] Routine updatedRoutine)
+    {
+        var existingRoutine = _context.Routines.Find(id);
+
+        if (existingRoutine == null)
+        {
+            return NotFound();
+        }
+
+  
+        existingRoutine.Title = updatedRoutine.Title;
+        existingRoutine.Description = updatedRoutine.Description;
+
+        _context.SaveChanges();
+
+        return Ok(existingRoutine);
+    }
+}
